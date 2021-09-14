c9a50a0c13644896df00a4f57752f1cc8cd0cd46
RockiJunior <gabrielvillarroel_11@hotmail.com>
2021-09-13 22:55:12 -0300

    B& & DB test

--- PI-Videogames-main/api/tests/models/videogame.spec.js
+++ PI-Videogames-main/api/tests/models/videogame.spec.js
@@ -13,6 +13,22 @@ const {
     Videogame
 } = connection(TEST_DB_NAME);
 
+const data = {
+    valid: {
+        id: '96c1ee7d-cc4c-4155-bab7-ff0f7777ed5c',
+        name: 'Super Mario Bros',
+        createdInDB: true,
+        image: "https://media.rawg.io/media/games/a8b/a8bf6f31bfbdaf7d4b86c1953c62cee0.jpg",
+        description: "hola que tal1",
+        released: "1994-07-04",
+        rating: 5.2,
+        platforms: "PC",
+    },
+    invalid: {
+
+    }
+}
+
 describe('Videogame model', () => {
     before(() => {
 
@@ -21,21 +37,27 @@ describe('Videogame model', () => {
                 console.error('Unable to connect to the database:', err);
             })
     });
+
     describe('Validators', () => {
         beforeEach(() => Videogame.sync({
             force: true
         }));
-        describe('name', () => {
-            it('should throw an error if name is null', (done) => {
-                Videogame.create({})
-                    .then(() => done(new Error('It requires a valid name')))
-                    .catch(() => done());
-            });
-            it('should work when its a valid name', () => {
-                Recipe.create({
-                    name: 'Super Mario Bros'
-                });
-            });
+
+        it('should throw an error if data is invalid', (done) => {
+            Videogame.create(data.invalid)
+                .then(() => done(new Error('It requires a valid data')))
+                .catch(() => done());
         });
+
+        it('should create to videogame when its a valid data', async() => {
+            await Videogame.create(data.valid);
+            const videogame = await Videogame.findAll({
+                where: {
+                    id: '96c1ee7d-cc4c-4155-bab7-ff0f7777ed5c'
+                }
+            });
+            expect(videogame.length).to.equal(1);
+        });
+
     });
 });
--- PI-Videogames-main/api/tests/routes/videogame.spec.js
+++ PI-Videogames-main/api/tests/routes/videogame.spec.js
@@ -1,24 +1,83 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
+const axios = require('axios');
+const sinon = require('sinon');
+
+const {
+    expect
+} = require('chai');
+
+const {
+    connection
+} = require('../../src/db.js');
+
 const session = require('supertest-session');
+
 const app = require('../../src/app.js');
-const { Videogame, conn } = require('../../src/db.js');
+
+const {
+    TEST_DB_NAME
+} = process.env;
+
+const {
+    conn,
+    Videogame
+} = connection(TEST_DB_NAME);
 
 const agent = session(app);
-const videogame = {
-  name: 'Super Mario Bros',
-};
+
+const data = {
+    valid: {
+        id: '96c1ee7d-cc4c-4155-bab7-ff0f7777ed5c',
+        name: 'Super Mario Bros',
+        createdInDB: true,
+        image: "https://media.rawg.io/media/games/a8b/a8bf6f31bfbdaf7d4b86c1953c62cee0.jpg",
+        description: "hola que tal1",
+        released: "1994-07-04",
+        rating: 5.2,
+        platforms: "PC",
+    },
+    invalid: {
+
+    }
+}
+
+const rawgResponse = {
+    data: {
+        count: 612416,
+        next: "",
+        previous: null,
+        results: []
+    }
+}
+
 
 describe('Videogame routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Videogame.sync({ force: true })
-    .then(() => Videogame.create(videogame)));
-  describe('GET /videogames', () => {
-    it('should get 200', () =>
-      agent.get('/videogames').expect(200)
-    );
-  });
+    let stub;
+
+    before(() => {
+        conn.authenticate()
+            .catch((err) => {
+                console.error('Unable to connect to the database:', err);
+            })
+    });
+
+    beforeEach(() => {
+        Videogame.sync({
+                force: true
+            })
+            .then(() => Videogame.create(data.valid))
+        stub = sinon.stub(axios, 'get').returns(Promise.resolve(rawgResponse));
+    });
+
+    afterEach(() => {
+        stub.restore();
+    })
+
+    describe('GET /videogames', () => {
+        it('should get 200', async() => {
+            const res = await agent.get('/videogames');
+            expect(res.status).to.equal(200);
+            sinon.assert.callCount(stub, 5);
+        });
+    });
 });
